3b1a7465e4da7bb1b040bfc2d458f5c566664648
Brett New <brett.new91@gmail.com>
2016-07-15 16:54:13 -0700

    Add first integration spec to allow user to input stylist.

--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -4,11 +4,13 @@ require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('the input stylist path', {:type => :feature}) do
-#   it('allows the user to add a stylist and then to display that stylist in a list') do
-#     visit('/stylists')
-#     fill_in('stylist_name', :with => 'Sarah')
-#     click_button('Add Stylist')
-#     expect(page).to have_content('Sarah')
-#   end
-# end
+describe('the input stylist path', {:type => :feature}) do
+  it('allows the user to add a stylist and then to display that stylist in a list') do
+    visit('/')
+    click_link('View All Stylists')
+    visit('/stylists/view')
+    fill_in('stylist_name', :with => 'Sarah')
+    click_button('Add Stylist')
+    expect(page).to have_content('Sarah')
+  end
+end
