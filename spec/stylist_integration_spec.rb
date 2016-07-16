b7b18d33ca321e7b3c4347e3f8f8639c0bf4fe10
Brett New <brett.new91@gmail.com>
2016-07-15 17:06:52 -0700

    Add integration specs to add and view stylists.

--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -4,13 +4,30 @@ require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the input stylist path', {:type => :feature}) do
-  it('allows the user to add a stylist and then to display that stylist in a list') do
+describe('the view stylists path', {:type => :feature}) do
+  it('allows the user to visit home page and view stylists') do
     visit('/')
     click_link('View All Stylists')
+    expect(page).to have_content('There are no stylists yet')
+  end
+end
+
+describe('the add stylists path', {:type => :feature}) do
+  it('allows the user to enter stylist and view that stylist') do
     visit('/stylists/view')
     fill_in('stylist_name', :with => 'Sarah')
     click_button('Add Stylist')
     expect(page).to have_content('Sarah')
   end
+end
+
+describe('the view stylists path', {:type => :feature}) do
+  it('allows the user to view added stylist') do
+    visit('/stylists/view')
+    fill_in('stylist_name', :with => 'Sarah')
+    click_button('Add Stylist')
+    click_link('Sarah')
+    expect(page).to have_content ('Update Sarah')
+  end
+
 end
