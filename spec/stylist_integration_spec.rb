03b2892e82560f284e6960292a44eb0eac43e942
Brett New <brett.new91@gmail.com>
2016-07-15 17:24:10 -0700

    add integration spec to add client to a stylist path

--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -53,3 +53,17 @@ describe('the delete stylist path', {:type => :feature}) do
     expect(page).to have_content('There are no stylists')
   end
 end
+
+describe('the add a client path', {:type => :feature}) do
+  it('allows the user to assign a client to an existing stylist') do
+  visit('/stylists/view')
+  fill_in('stylist_name', :with => 'Sarah')
+  click_button('Add Stylist')
+  click_link('Sarah')
+  fill_in('client_name', :with => 'Bob')
+  fill_in('phone_number', :with => '555-5555')
+  fill_in('email', :with => 'bob@email.com')
+  click_button('Add Client')
+  expect(page).to have_content('Bob')
+  end
+end
